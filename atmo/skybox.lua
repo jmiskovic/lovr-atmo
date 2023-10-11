003cc36c0c627e5a7f904f7d1aa54232d1d1989f
Josip Miskovic <josipmiskovic@gmail.com>
2023-10-11 17:42:24 +0200

    Update the readback method
    Transfer pass is no longer part of lovr, and lg.getPass is also
    depricated.

--- atmo/skybox.lua
+++ atmo/skybox.lua
@@ -1,6 +1,15 @@
 local m = {}
 m.__index = m
 
+local cubemap_transforms = {
+  Mat4():lookAt(vec3(), vec3( 1, 0, 0), vec3(0, 1, 0)),
+  Mat4():lookAt(vec3(), vec3(-1, 0, 0), vec3(0, 1, 0)),
+  Mat4():lookAt(vec3(), vec3( 0, 1, 0), vec3(0, 0,-1)),
+  Mat4():lookAt(vec3(), vec3( 0,-1, 0), vec3(0, 0, 1)),
+  Mat4():lookAt(vec3(), vec3( 0, 0, 1), vec3(0, 1, 0)),
+  Mat4():lookAt(vec3(), vec3( 0, 0,-1), vec3(0, 1, 0))
+}
+
 function m.new(resolution)
   local self = setmetatable({}, m)
   self.resolution = math.floor(resolution or 256)
@@ -15,35 +24,43 @@ end
 
 function m:bake(drawfn)
   -- render scene to cubemap texture
-  local render_pass = lovr.graphics.getPass('render', {self.cubetex, samples=1, mipmap=false})
+  local render_pass = lovr.graphics.newPass({self.cubetex, samples=1})
   local projection = mat4():perspective(math.pi / 2, 1, 0, 0)
-  local transforms = {
-    mat4():lookAt(vec3(), vec3( 1, 0, 0), vec3(0, 1, 0)),
-    mat4():lookAt(vec3(), vec3(-1, 0, 0), vec3(0, 1, 0)),
-    mat4():lookAt(vec3(), vec3( 0, 1, 0), vec3(0, 0,-1)),
-    mat4():lookAt(vec3(), vec3( 0,-1, 0), vec3(0, 0, 1)),
-    mat4():lookAt(vec3(), vec3( 0, 0, 1), vec3(0, 1, 0)),
-    mat4():lookAt(vec3(), vec3( 0, 0,-1), vec3(0, 1, 0))
-  }
-  for i, transform in ipairs(transforms) do
+  for i, transform in ipairs(cubemap_transforms) do
     render_pass:setProjection(i, projection)
     render_pass:setViewPose(i, transform)
   end
   drawfn(render_pass)
+  lovr.graphics.submit(render_pass)
   -- read back the horizon color
-  local transfer_pass = lovr.graphics.getPass('transfer')
-  local readback = transfer_pass:read(self.cubetex, 0, 0, 1)
-  lovr.graphics.submit(render_pass, transfer_pass)
-  readback:wait()
-  local image = readback:getImage()
-  -- sample rendered image at three points along the horizon
-  local r1, g1, b1 = image:getPixel(0, math.floor(self.resolution / 2))
-  local r2, g2, b2 = image:getPixel(math.floor(self.resolution / 2), math.floor(self.resolution / 2))
-  local r3, g3, b3 = image:getPixel(math.floor(self.resolution - 1), math.floor(self.resolution / 2))
+  local readbacks = {
+    self.cubetex:newReadback(0, 0, 1, 1),
+    self.cubetex:newReadback(0, 0, 2, 1),
+    self.cubetex:newReadback(0, 0, 3, 1),
+    self.cubetex:newReadback(0, 0, 4, 1),
+    self.cubetex:newReadback(0, 0, 5, 1),
+    self.cubetex:newReadback(0, 0, 6, 1)
+  }
+  for i, readback in ipairs(readbacks) do
+    readback:wait()
+  end
+
+  self.images = {}
+  for i, readback in ipairs(readbacks) do
+    readback:wait()
+    self.images[i] = readback:getImage()
+    --lovr.filesystem.write('img'..i..'.png', self.images[i]:encode())
+  end
+
+  -- sample rendered images at four points along the horizon
+  local r1, g1, b1 = self.images[1]:getPixel(math.floor(self.resolution / 2), math.floor(self.resolution / 2))
+  local r2, g2, b2 = self.images[2]:getPixel(math.floor(self.resolution / 2), math.floor(self.resolution / 2))
+  local r3, g3, b3 = self.images[5]:getPixel(math.floor(self.resolution / 2), math.floor(self.resolution / 2))
+  local r4, g4, b4 = self.images[6]:getPixel(math.floor(self.resolution / 2), math.floor(self.resolution / 2))
   -- take harmonic mean, to remove outliers such as sun
-  self.horizon_color:set(3 / (1 / r1 + 1 / r2 + 1 / r3),
-                         3 / (1 / g1 + 1 / g2 + 1 / g3),
-                         3 / (1 / b1 + 1 / b2 + 1 / b3))
+  self.horizon_color:set(4 / (1 / r1 + 1 / r2 + 1 / r3 + 1 / r4),
+                         4 / (1 / g1 + 1 / g2 + 1 / g3 + 1 / g4),
+                         4 / (1 / b1 + 1 / b2 + 1 / b3 + 1 / b4))
 end
 
 
